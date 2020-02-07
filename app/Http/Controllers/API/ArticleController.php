7be92a4735b5f7e8264ead83e6dee422f122953c
nishitha1101 <nishitha@ipropal.com>
2020-02-07 16:38:48 +0530

    make the update changes to tags create asset table,pivot table

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -32,6 +32,13 @@ class ArticleController extends BaseController
     public function store(Request $request)
     {
         $input = $request->all();
+        $aid =$request->user_id;
+        $usid = auth()->user()->id;
+
+        if($aid!=$usid)
+        {
+            return $this->sendError('Sorry, create an article with your own ID.');
+        }
 
         $article = Article::create($input);
 
@@ -39,6 +46,7 @@ class ArticleController extends BaseController
             'title' => 'required',
             'body' => 'required',
             'user_id' => 'required',
+            'tags' => 'required',
             'featured_image_url' => 'url'
         ]);
 
@@ -90,17 +98,15 @@ class ArticleController extends BaseController
     public function update(Request $request, Article $article)
     {
         $input = $request->all();
-
-
         $aid =$request->user_id;
         $usid = auth()->user()->id;
 
         if($aid!=$usid)
         {
-            //return $this->sendError('You cannot update this Article.');
             return $this->sendError('Sorry, edit your own article.');
-
         }
+
+        $article->tags()->detach();
 
         $validator = Validator::make($input, [
             'title' => 'required',
@@ -112,15 +118,30 @@ class ArticleController extends BaseController
             return $this->sendError('Validation Error.', $validator->errors());
         }
 
+        $tags_string = $input['tags'];
+        $tags_array = explode(',', $tags_string);
+
+        $tag_id_array = array();
+
+
+        foreach($tags_array as $tag_all) {
+            $tag = new App\Tag;
+            $tag->name = $tag_all;
+            $tag->save();
+
+            array_push($tag_id_array, $tag->id);
+        }
+
+        $article->tags()->attach($tag_id_array);
+
         $article->title = $input['title'];
         $article->body = $input['body'];
+        $article->tags = $input['tags'];
         $article->save();
 
         $article->update($this->validateArticle());
 
         return $this->sendResponse(new ArticleResource($article), 'Article updated successfully.');
-
-       // return $this->sendResponse(new ArticleResource($article), 'Article updated successfully.');
     }
 
     /**
@@ -143,7 +164,7 @@ class ArticleController extends BaseController
             'title' => 'required',
             'body' => 'required',
             'user_id' => 'required',
-
+            'tags' => 'required',
             'featured_image_url' => 'url'
 
         ]);
